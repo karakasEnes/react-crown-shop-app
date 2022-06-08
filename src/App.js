d46677aba57459b35eddcf5754c8a9e5337265c4
Enes Karakaş <enes.karakas.e@gmail.com>
2022-06-09 00:23:46 +0300

    react router added, components arranged for that purpose

--- src/App.js
+++ src/App.js
@@ -1,36 +1,11 @@
-import './App.css';
-import './categories.styles.scss';
-import CategoriesList from './components/categories-list/categories-list.component';
-
+import Home from './routes/home/home.components';
+import { Routes, Route } from 'react-router-dom';
 function App() {
-  const categories = [
-    {
-      id: 1,
-      title: 'hats',
-      imageUrl: 'https://i.ibb.co/cvpntL1/hats.png',
-    },
-    {
-      id: 2,
-      title: 'jackets',
-      imageUrl: 'https://i.ibb.co/px2tCc3/jackets.png',
-    },
-    {
-      id: 3,
-      title: 'sneakers',
-      imageUrl: 'https://i.ibb.co/0jqHpnp/sneakers.png',
-    },
-    {
-      id: 4,
-      title: 'womens',
-      imageUrl: 'https://i.ibb.co/GCCdy8t/womens.png',
-    },
-    {
-      id: 5,
-      title: 'mens',
-      imageUrl: 'https://i.ibb.co/R70vBrQ/men.png',
-    },
-  ];
-  return <CategoriesList categories={categories} />;
+  return (
+    <Routes>
+      <Route path='/' element={<Home />} />
+    </Routes>
+  );
 }
 
 export default App;
