040b7fae2878b284498c258ec2f314b745a77118
Enes Karakaş <enes.karakas.e@gmail.com>
2022-06-09 23:21:42 +0300

    creating user via REDIRECT, just for experiment with firebase, we will remove it later, we will use popUp method not REDIRECT

--- src/routes/sign-in/sign-in.components.jsx
+++ src/routes/sign-in/sign-in.components.jsx
@@ -1,9 +1,28 @@
+import { useEffect } from 'react';
+import { getRedirectResult } from 'firebase/auth';
+
 import {
+  auth,
   signInWithGooglePopup,
+  signInWithGoogleRedirect,
   createUserDocumentFromAuth,
 } from '../../utils/firebase/firebase.utils';
 
 const SignIn = () => {
+  useEffect(() => {
+    const getResponse = async () => {
+      const response = await getRedirectResult(auth);
+      console.log(response);
+
+      if (response) {
+        //creating our user inside our DATABASE!
+        const userDocRef = await createUserDocumentFromAuth(response.user);
+      }
+    };
+
+    getResponse();
+  });
+
   const logGoogleUser = async () => {
     const response = await signInWithGooglePopup();
     console.log(response);
@@ -15,6 +34,9 @@ const SignIn = () => {
     <div>
       <h1>Sign In PAGE!!!</h1>;
       <button onClick={logGoogleUser}>sign In with Google</button>
+      <button onClick={signInWithGoogleRedirect}>
+        sign In with Google REDIRECT
+      </button>
     </div>
   );
 };
