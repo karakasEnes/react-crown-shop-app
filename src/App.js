8308855c6f840dcb96063c9399ec1704e0831870
Enes Karakaş <enes.karakas.e@gmail.com>
2022-06-09 16:00:09 +0300

    navigation added

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
 import Home from './routes/home/home.components';
-import { Routes, Route, Outlet } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
+import Navigation from './components/navigation/navigation.component';
 
 const Shop = () => {
   return <h1>I AM SHOP</h1>;
-};
-
-const Navigation = () => {
-  return (
-    <div>
-      <h1>I AM NAV !...</h1>;
-      <Outlet />
-    </div>
-  );
 };
 
 function App() {
   return (
     <Routes>
       <Route path='/' element={<Navigation />}>
-        <Route path='shop' element={<Shop />} />
         <Route index element={<Home />} />
+        <Route path='shop' element={<Shop />} />
       </Route>
     </Routes>
   );
